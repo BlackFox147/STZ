a1bb5acd593e675277998e2dedc906bfc88d6503
Yuliya Momatava <yuliya.momatava@gmail.com>
2019-10-17 23:18:04 +0300

    Создание проекта для второй лабораторной работы

---
+++ STZ_2/STZ_2/ShellBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+using STZ_2.ViewModels;
+
+namespace STZ_2
+{
+    public class ShellBootstrapper : BootstrapperBase
+    {
+        public ShellBootstrapper() : base(true)
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
